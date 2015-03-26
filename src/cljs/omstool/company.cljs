58b7c417e0de6f1197d35448c2c26d4eb4e50fd4
Eric Hogberg <misc@erichogberg.com>
2015-03-26 16:01:07 -0500

    Rudimentary company list table.

--- src/cljs/omstool/company.cljs
+++ src/cljs/omstool/company.cljs
@@ -1,15 +1,45 @@
 (ns omstool.company
   (:require [om.core :as om :include-macros true]
             [om-tools.dom :as dom :include-macros true]
+            [om-tools.core :refer-macros [defcomponent]]
+            [om-bootstrap.table :as t]
             [om-bootstrap.random :as r]))
 
 
-(defn render-companies-index [app owner]
-  (reify
-    om/IRender
-    (render [_]
-      (r/jumbotron nil
-                   (dom/p "Companies index")))))
+(def company-data [ {:sid "Foo"
+                     :name "Foo Company"}
+                    {:sid "Bar"
+                     :name "Bar Company"}])
+
+
+(defcomponent company-info [company _]
+  (init-state [_]
+              {:company company})
+  (render-state [_ {:keys [company]}]
+                (dom/tr
+                 (dom/td (:name company))
+                 (dom/td (:sid  company))
+                 (dom/td (dom/a {:href ""} "edit")))))
+
+
+(defcomponent companies-table [_ _]
+  (render-state [_ {:keys [companies]}]
+                (t/table {:striped? true}
+                         (dom/thead
+                          (dom/tr
+                           (dom/th "Company Name")
+                           (dom/th "SID")
+                           (dom/th "")))
+                         (dom/tbody
+                          (om/build-all company-info companies)))))
+
+
+(defcomponent render-companies-index [_ _]
+  (render [_]
+          (dom/div
+           (dom/h3 "Companies")
+           (om/build companies-table nil
+                     {:init-state {:companies company-data}}))))
 
 
 
